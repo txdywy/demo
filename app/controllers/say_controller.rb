a5f58f215c3e9993b9526bfb1161855fccb3aa51
Yi Wei <genius.ron@gmail.com>
2011-07-13 21:01:29 -0500

    new file:   .gitignore

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,10 @@
+class SayController < ApplicationController
+  def hello
+	@time=Time.now
+  end
+
+  def goodbye
+	@files = Dir.glob('*')
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'ac9b3eef9edb961a7f3634839bafcde0628ebf42dd371bdf3a72c4912341c66b078ea9f2b1b23419e4c920b28505679c52a037ccba5fbae23ca29b3c089ddb9b'
